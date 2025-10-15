ca2027d960ef500c9438066b7224389c87fc1545
Fern Potter <fernrp11@gmail.com>
2025-10-15 12:44:57 -0400

    updated .ino file

--- Embedded-dev/src/TestCode.ino
+++ Embedded-dev/src/TestCode.ino
@@ -1,7 +1,11 @@
 #include "PPGHandler.h"
-#include <cmath>
+
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
   Serial.begin(115200);
   Serial.println("Initializing...");
 
@@ -11,7 +15,7 @@ void setup() {
     Serial.println("MAX30105 was not found. Please check wiring/power. ");
     while (1);
   }
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  particleSensor.setup(BRIGHTNESS, SAMPLEAVE, LEDMODE, SAMPLERATE, PULSEWIDTH, ADCRANGE); //Configure sensor with these settings
 
   //Arduino plotter auto-scales annoyingly. To get around this, pre-populate
   //the plotter with 500 of an average reading from the sensor
@@ -28,12 +32,30 @@ void setup() {
   //Pre-populate the plotter so that the Y scale is close to IR values
   for (int x = 0 ; x < 500 ; x++)
     Serial.println(baseValue);
-  LOGLN("Press enter to begin: ");
-  while (!Serial.available()){
-  }
-  Serial.read();
 }
 
 void loop() {
-  testDriver2();
+  analogWrite(LED_RED, 0xff);
+  analogWrite(LED_GREEN, 0xff);
+  analogWrite(LED_BLUE, 0xff);
+  while (!Serial.available()){
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  }
+  while (Serial.available()){
+  Serial.read();
+  }
+  analogWrite(LED_RED, 0xaf);
+
+  //testRun(BRIGHTNESS, SAMPLEAVE, LEDMODE, SAMPLERATE, PULSEWIDTH, ADCRANGE, TIME);
+  // To run a measurement, call the above function as is with the desired parameters.
+  // It will repeat, asking for a keypress to start each time.
+  // if no parameters are given, the defaults defined at the top of PPGHandler.h file will be used.
+  // The last parameter is the time in seconds to measure for, total measurement time over 3 minutes has not been tested.
+  // After the last measurement is complete the data will be printed to serial in CSV format.
+  // The data can be copied from the serial monitor and pasted into a text file, or logged via a program such as teraterm.
+
+
   }
